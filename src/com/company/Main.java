59d451d31bb8bc25680a67b054af4433f5aad83d
koebsch <matthieu.koebsch@edu.itescia.fr>
2020-10-07 13:02:47 +0200

    fonction ligne

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,12 @@
 package com.company;
 
-import java.sql.ResultSet;
-import java.util.SortedMap;
-
 public class Main {
     public static final String RED = "\033[0;31m";
     public static final String RESET = "\033[0m";
     static int[][] boardEasy = {
             {4, 0, 0, 1, 0, 2, 6, 8, 0},
             {1, 0, 0, 0, 9, 0, 0, 0, 4},
-            {0,  3, 8, 0, 6, 4, 0, 1, 0},
+            {0, 3, 8, 0, 6, 4, 0, 1, 0},
             {0, 0, 5, 0, 7, 1, 9, 2, 0},
             {0, 2, 6, 0, 0, 9, 8, 0, 0},
             {8, 0, 0, 2, 5, 0, 0, 0, 0},
@@ -17,45 +14,66 @@ public class Main {
             {2, 5, 0, 6, 0, 0, 1, 0, 7},
             {6, 0, 7, 9, 0, 5, 3, 0, 0}
     };
+    public static void afficheGrille(int[][] tableau) {
 
-
-    public static void afficheGrille(int[][] tableau){
-
-        for(int colonne = 0; colonne < 9 ;colonne++){
-            if(colonne%3 == 0){
-                System.out.print(RED +"______________________________________"+ RESET);
+        for (int colonne = 0; colonne < 9; colonne++) {
+            if (colonne % 3 == 0) {
+                System.out.print(RED + "______________________________________" + RESET);
                 System.out.println();
-            }else{
+            } else {
                 System.out.print("______________________________________");
                 System.out.println();
             }
 
-            for(int ligne =0;ligne<9;ligne++){
+            for (int ligne = 0; ligne < 9; ligne++) {
 
-                if(ligne%3 == 0){
-                    System.out.print(RED+" | "+RESET);
-                }else{
+                if (ligne % 3 == 0) {
+                    System.out.print(RED + " | " + RESET);
+                } else {
                     System.out.print(" | ");
                 }
                 System.out.print(tableau[colonne][ligne]);
 
-            }System.out.print(RED+" | "+RESET);
+            }
+            System.out.print(RED + " | " + RESET);
             System.out.println("");
 
-        }System.out.print(RED +"______________________________________"+ RESET);
-
-
+        }
+        System.out.print(RED + "______________________________________" + RESET);
 
 
     }
 
+    static boolean ligne(int[][] board){
+        int ligne=0;
+        int colonne=0;
+        int colonneplus=colonne+1;
+        while (ligne<9){
+            if (board[ligne][colonne] == board[ligne][colonneplus]   &&   board[ligne][colonne] != 0){
+                return false;
+            }
+            else{
+                colonneplus=colonneplus+1;
+            }
+            if (colonneplus==8){
+                colonne=colonne+1;
+                colonneplus=colonne+1;
+            }
+            if(colonne==7){
+                colonne=0;
+                ligne=ligne+1;
+                colonneplus=colonne+1;
+            }
+        }
+        return true;
+    }
+
 
     public static void main(String[] args) {
-    afficheGrille(boardEasy);
-
-
-
-
+        afficheGrille(boardEasy);
+        int [][] board=boardEasy;
+        boolean ligneverifier=ligne(board);
+        System.out.println(ligneverifier);
 
 
 

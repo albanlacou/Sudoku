af1349ba176ff6bab5e5802e476f1688fc7f5022
Admin <lacou.alban@gmail.com>
2020-10-07 12:30:57 +0200

    commit l'afichage terminer

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,11 @@
 package com.company;
 
+import java.sql.ResultSet;
 import java.util.SortedMap;
 
 public class Main {
-
+    public static final String RED = "\033[0;31m";
+    public static final String RESET = "\033[0m";
     static int[][] boardEasy = {
             {4, 0, 0, 1, 0, 2, 6, 8, 0},
             {1, 0, 0, 0, 9, 0, 0, 0, 4},
@@ -19,14 +21,30 @@ public class Main {
 
     public static void afficheGrille(int[][] tableau){
 
-        for(int n = 0; n < 9 ;n++){
-            for(int i =0;i<9;i++){
-                System.out.print(tableau[n][i]);
-                System.out.print(" | ");
+        for(int colonne = 0; colonne < 9 ;colonne++){
+            if(colonne%3 == 0){
+                System.out.print(RED +"______________________________________"+ RESET);
+                System.out.println();
+            }else{
+                System.out.print("______________________________________");
+                System.out.println();
             }
+
+            for(int ligne =0;ligne<9;ligne++){
+
+                if(ligne%3 == 0){
+                    System.out.print(RED+" | "+RESET);
+                }else{
+                    System.out.print(" | ");
+                }
+                System.out.print(tableau[colonne][ligne]);
+
+            }System.out.print(RED+" | "+RESET);
             System.out.println("");
 
-        }
+        }System.out.print(RED +"______________________________________"+ RESET);
+
+
 
 
     }
